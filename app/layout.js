82103ec5d34f4e238cf282b6bf9273087fb9d810
Joe Wang <69328902+Storce@users.noreply.github.com>
2025-03-28 17:35:11 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ export default function RootLayout({ children }) {
 
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.ico" />
+            </head>
             <body style={{ backgroundColor: '#F5EEDC', color: 'black' }}>
                 <nav
                     style={{
